885111423284f2774f1eb58944d8108ce4bcb144
Tianon Gravi <admwiggin@gmail.com>
2016-02-02 16:46:28 -0800

    Add a "pretty" version of the JSON now that it's compressed :smile:

--- update.sh
+++ update.sh
@@ -5,3 +5,4 @@ cd "$(dirname "$(readlink -f "$BASH_SOURCE")")"
 
 set -x
 curl -fSL'#' 'https://security-tracker.debian.org/tracker/data/json' -o debian-security.json
+jq . debian-security.json > debian-security.pretty.json
